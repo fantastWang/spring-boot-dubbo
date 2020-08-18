6bff7ce922aa8a7c1f7526d50de660b11b481df0
fantast <1061505447@qq.com>
2020-08-18 20:29:58 +0800

    提交多协议

--- spring-boot-dubbo-provider/src/main/java/com/it/wcj/springbootdubboprovider/SayServiceImpl.java
+++ spring-boot-dubbo-provider/src/main/java/com/it/wcj/springbootdubboprovider/SayServiceImpl.java
@@ -7,7 +7,7 @@ import org.apache.dubbo.config.annotation.DubboService;
  * @Description TODO
  * @Date 2020/8/7 16:15
  **/
-@DubboService(registry = {"shanghai"})
+@DubboService(registry = {"shanghai"},protocol = {"dubbo","rest"})
 public class SayServiceImpl implements ISayService {
     @Override
     public String sayHello(String msg) {
